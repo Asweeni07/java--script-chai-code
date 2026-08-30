1715b39f996f630c5fc4437992461c84a918ae63
Asweeni07 <asweenikumar993484@gmail.com>
2026-08-30 05:49:09 +0000

    conversions

---
+++ basics/ConversionOperations.js
@@ -0,0 +1,119 @@
+// CASE 1.
+
+// let score = 33
+
+// console.log(typeof score);    //number
+
+
+// CASE 2.
+
+
+// let score = "33"   
+
+// console.log(typeof score);    //string
+
+
+// CONVERSION
+
+// let score = "33abc"
+
+// console.log(typeof score);       //string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);         //Not a Number
+
+
+// CONVERSION OF NULL
+
+// let score = null
+
+// console.log(typeof score);       //object
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);         // 0 (zero)
+
+// CONVERSION of undefined
+
+// let score = undefined
+
+// console.log(typeof score);       //undefined
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);         //Not a Number
+
+
+// CONVERSION OF BOOLEAN
+
+// let score = true
+
+// console.log(typeof score);       //Boolean
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);         // 1
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+//points to remember
+
+// 1 => true; 0 = false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 24
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
